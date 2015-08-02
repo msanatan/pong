4bcfb4621a345b531bc4d2e132de3396b980753d
Marcus Sanatan <msanatan@gmail.com>
2015-08-02 16:04:40 -0400

    Setup basic canvas properties

---
+++ javascripts/pong.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+  var canvas = document.getElementById('game');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var ctx = canvas.getContext('2d');
+  var FPS = 1000 / 60;
+
+  var draw = function () {
+
+  };
+
+  draw();
+})();
