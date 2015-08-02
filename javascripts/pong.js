f63b1cd62f823d89e62d559ef5e86a70f504126c
Marcus Sanatan <msanatan@gmail.com>
2015-08-02 16:32:44 -0400

    Define paddle object and a basic render function. Also capitalise width and height constants to easily distinguish them from object properties or parameters

--- javascripts/pong.js
+++ javascripts/pong.js
@@ -1,15 +1,29 @@
 (function() {
   'use strict';
   var canvas = document.getElementById('game');
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
+  var WIDTH = window.innerWidth;
+  var HEIGHT = window.innerHeight;
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
   var ctx = canvas.getContext('2d');
   var FPS = 1000 / 60;
 
+  var Paddle = function(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+  };
+
+  Paddle.prototype.render = function() {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
   var clear = function() {
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
   };
 
   var update = function() {
@@ -17,7 +31,8 @@
   };
 
   var render = function () {
-
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
   };
 
   var step = function() {
