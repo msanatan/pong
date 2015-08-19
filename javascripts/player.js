111823fc19360c3a2d449ebbe6db3ee13646d90d
Marcus Sanatan <msanatan@gmail.com>
2015-08-19 18:12:03 -0300

    Generalise player object for easier setting up

--- javascripts/player.js
+++ javascripts/player.js
@@ -1,18 +1,18 @@
 var Player;
 
-Player = function(canvasWidth, canvasHeight) {
-  var playerWidth, playerHeight, paddle;
-  playerHeight = 100;
-  this.canvasWidth = canvasWidth;
-  this.canvasHeight = canvasHeight;
+Player = function(options) {
+  var paddle;
+  this.canvasWidth = options.canvasWidth || 1280;
+  this.canvasHeight = options.canvasHeight || 640;
   paddle = {
-    x: 10,
-    y: this.canvasHeight / 2 - (playerHeight / 2),
-    width: 10,
-    height: playerHeight
+    x: options.paddle.x || 10,
+    y: options.paddle.y || this.canvasHeight / 2 - (options.paddle.height / 2),
+    width: options.paddle.width || 10,
+    height: options.paddle.height || 100
   };
   this.paddle = new Paddle(paddle);
   this.score = 0;
+  this.colour = options.colour || '#FFFFFF';
 };
 
 Player.prototype.render = function(context) {
@@ -20,7 +20,7 @@ Player.prototype.render = function(context) {
 
   // Print score on screen
   context.font = 'bold 80px Helvetica, Verdana, san-serif';
-  context.fillStyle = '#FFFFFF';
+  context.fillStyle = this.colour;
   context.fillText(this.score, (this.canvasWidth / 2) - 80, 80);
 };
 
