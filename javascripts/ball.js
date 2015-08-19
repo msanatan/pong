6ca38d958b810d6987f31127940b068936e00862
Marcus Sanatan <msanatan@gmail.com>
2015-08-19 17:24:38 -0300

    Generalise ball object so it can be customised from outside easier

--- javascripts/ball.js
+++ javascripts/ball.js
@@ -1,17 +1,18 @@
 var Ball;
 
-Ball = function(x, y, radius) {
-  this.x = x;
-  this.y = y;
-  this.radius = radius;
-  this.xSpeed = -3;
-  this.ySpeed = 0;
+Ball = function(options) {
+  this.x = options.x || 640;
+  this.y = options.y || 320;
+  this.radius = options.radius || 10;
+  this.xSpeed = options.xSpeed || -3;
+  this.ySpeed = options.ySpeed || 0;
+  this.colour = options.colour || 'skyblue';
 };
 
 Ball.prototype.render = function(context) {
   context.beginPath();
   context.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-  context.fillStyle = 'skyblue';
+  context.fillStyle = this.colour;
   context.fill();
 };
 
