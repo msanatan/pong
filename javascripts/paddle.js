ed752bd0a073ed221439325b6c02cecafcb75fdb
Marcus Sanatan <msanatan@gmail.com>
2015-08-19 17:50:31 -0300

    Generalise paddle object to be consistent with other objects

--- javascripts/paddle.js
+++ javascripts/paddle.js
@@ -1,13 +1,13 @@
 // Define game objects, i.e paddle and ball
 var Paddle;
 
-Paddle = function(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.xSpeed = 0;
-  this.ySpeed = 0;
+Paddle = function(options) {
+  this.x = options.x || 20;
+  this.y = options.y || 320;
+  this.width = options.width || 10;
+  this.height = options.height || 50;
+  this.xSpeed = options.xSpeed || 0;
+  this.ySpeed = options.ySpeed || 0;
 };
 
 Paddle.prototype.move = function(x, y, height) {
