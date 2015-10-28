38e9cb291e43a436185f2830ff29d33fdf75dfec
Marcus Sanatan <msanatan@gmail.com>
2015-10-28 14:42:06 -0400

    Animation refactoring
    Logical refactoring to fit more with usual game design. Change the step function into an engine method as it essentially calls update and render. Keep the animation function in global scope because it stores window functions. And then create a self-invoking function which handles the animation and stepping. Also rename the GameEngine object to simply just Engine

--- javascripts/engine.js
+++ javascripts/engine.js
@@ -1,6 +1,6 @@
-var GameEngine, WIDTH, HEIGHT, FPS, engine, mainMenu, step;
+var Engine, WIDTH, HEIGHT, FPS, engine, mainMenu, step;
 
-GameEngine = function(canvasID, width, height, FPS) {
+Engine = function(canvasID, width, height, FPS) {
   'use strict';
   this.canvas = document.getElementById(canvasID);
   this.canvas.width = width;
@@ -8,7 +8,7 @@ GameEngine = function(canvasID, width, height, FPS) {
   this.FPS = FPS;
 };
 
-GameEngine.prototype.init = function() {
+Engine.prototype.init = function() {
   'use strict';
   this.canvas.focus();
   this.ctx = this.canvas.getContext('2d');
@@ -16,45 +16,42 @@ GameEngine.prototype.init = function() {
   this.inputHandler.register(this.canvas);
 };
 
-GameEngine.prototype.register = function(screen) {
+Engine.prototype.register = function(screen) {
   'use strict';
   this.screen = screen;
 };
 
-GameEngine.prototype.update = function() {
+Engine.prototype.update = function() {
   'use strict';
   this.inputHandler.update();
   this.screen.update(this.inputHandler);
 };
 
-GameEngine.prototype.render = function() {
+Engine.prototype.render = function() {
   'use strict';
   this.screen.render(this.ctx);
 };
 
-GameEngine.prototype.animate = (window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function(callback) {
-    window.setTimeout(callback, 1000 / this.FPS);
-  }).bind(this);
-
-GameEngine.prototype.stop = function() {
+Engine.prototype.stop = function() {
   this.screen = null;
-  this.animate.bind(this, function() {});
 };
 
-GameEngine.prototype.switchScreens = function(newScreen) {
+Engine.prototype.switchScreens = function(newScreen) {
   this.stop();
   this.inputHandler.reset();
   this.register(newScreen);
-  this.animate.bind(this, step);
+};
+
+Engine.prototype.step = function() {
+  'use strict';
+  engine.update();
+  engine.render();
 };
 
 WIDTH = window.innerWidth;
 HEIGHT = window.innerHeight;
 FPS = 60;
-engine = new GameEngine('game', WIDTH, HEIGHT, FPS);
+engine = new Engine('game', WIDTH, HEIGHT, FPS);
 engine.init();
 mainMenu = MainMenu({
   width: WIDTH,
@@ -63,11 +60,18 @@ mainMenu = MainMenu({
 });
 engine.register(mainMenu);
 
-step = function() {
-  'use strict';
-  engine.update();
-  engine.render();
-  engine.animate(step);
-};
 
-engine.animate(step);
+animate = (window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / this.FPS);
+  });
+
+(function() {
+  function main() {
+    animate(main);
+    engine.step();
+  };
+  main();
+})();
